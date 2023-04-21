cc66019106deff9b22905d273c368efd01e6f59b
Cami <cbrun.mine@gmail.com>
2023-04-21 11:06:09 -0600

    Created stubs for access page

--- src/games/model/chess/ChessAI.java
+++ src/games/model/chess/ChessAI.java
@@ -2,5 +2,19 @@ package games.model.chess;
 
 public class ChessAI
 {
+	public String getNextMove()
+	{
+		String fen = createFEN(true);
+		return accessPage("https://www.chessdb.cn/cdb.php?action=querybest&board=" + fen)
+	}
 	
+	private String createFEN(boolean isWebReady)
+	{
+		String space = " ";
+		if (isWebReady)
+		{
+			space = "%20";
+		}
+		return space;
+	}
 }
