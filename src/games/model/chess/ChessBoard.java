30250759d1908196a0a4faa5e3dc82c47c2ae901
Cami <cbrun.mine@gmail.com>
2023-04-25 11:58:24 -0600

    now initializes the chess board with the default chess layout

--- src/games/model/chess/ChessBoard.java
+++ src/games/model/chess/ChessBoard.java
@@ -17,7 +17,52 @@ public class ChessBoard
 	
 	private void setupBoard()
 	{
-		
+		for (int row = 0; row < board.length; row++)
+		{
+			for (int col = 0; col < board[0].length; col++)
+			{
+				int team = ChessPiece.BLACK;
+				if (row > board.length/2)
+				{
+					team = ChessPiece.WHITE;
+				}
+				
+				if (row == 1 || row == 6)
+				{
+					board[row][col] = new ChessPawn(team);
+				}
+				else if (row == 0 || row == 7)
+				{
+					switch (col)
+					{
+					case 0:
+					case 7:
+						board[row][col] = new ChessRook(team);
+						break;
+					case 1:
+					case 6:
+						board[row][col] = new ChessKnight(team);
+						break;
+					case 2:
+					case 5:
+						board[row][col] = new ChessBishop(team);
+						break;
+					case 3:
+						board[row][col] = new ChessKing(team);
+						break;
+					case 4:
+						board[row][col] = new ChessQueen(team);
+						break;
+					default:
+						board[row][col] = new ChessEmpty(ChessPiece.NOTEAM);
+					}
+				}
+				else
+				{
+					board[row][col] = new ChessEmpty(ChessPiece.NOTEAM);
+				}
+			}
+		}
 	}
 	
 	
