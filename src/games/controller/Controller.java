edc21c24902cbdf4ffe153eabb75bb3b644e4fdd
Cami <cbrun.mine@gmail.com>
2023-04-25 12:06:34 -0600

    Fixed some fen formatting inconsistancies

--- src/games/controller/Controller.java
+++ src/games/controller/Controller.java
@@ -1,16 +1,22 @@
 package games.controller;
 
 import games.model.chess.ChessAI;
+import games.model.chess.ChessBoard;
 
 import javax.swing.JOptionPane;
 
 public class Controller
 {
 	private ChessAI ai;
+	private ChessBoard board;
+	
 	public void start()
 	{
 		ai = new ChessAI(this);
 		ai.getNextMove();
+		
+		board = new ChessBoard();
+		System.out.println(board.getAsFEN());
 	}
 	
 	public void handleError(Exception error)
