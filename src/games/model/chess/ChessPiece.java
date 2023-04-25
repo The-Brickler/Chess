84d6174b2c7124523dac9e11634f3ce0b6330096
Cami <cbrun.mine@gmail.com>
2023-04-25 11:52:12 -0600

    created methods to help get fen number

--- src/games/model/chess/ChessPiece.java
+++ src/games/model/chess/ChessPiece.java
@@ -1,6 +1,17 @@
 package games.model.chess;
 
-public class ChessPiece
+public abstract class ChessPiece
 {
-
+	public final static int BLACK = -1;
+	public final static int WHITE = 1;
+	public final static int NOTEAM = 0;
+	
+	private int team;
+	
+	public ChessPiece(int team)
+	{
+		this.team = team;
+	}
+	
+	public abstract String getAsChar();
 }
--- src/games/model/chess/ChessQueen.java
+++ src/games/model/chess/ChessQueen.java
@@ -2,5 +2,23 @@ package games.model.chess;
 
 public class ChessQueen extends ChessPiece
 {
-
+private int team;
+	
+	public ChessQueen(int team)
+	{
+		super(team);
+		this.team = team;
+	}
+	
+	public String getAsChar()
+	{
+		String letter = "q";
+		
+		if (team == ChessPiece.WHITE)
+		{
+			letter = letter.toUpperCase();
+		}
+		
+		return letter;
+	}
 }
