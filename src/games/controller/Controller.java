5b01be41d6c6309551dcbaf3da33c10f6c4480b5
Cami <cbrun.mine@gmail.com>
2023-05-01 11:52:11 -0600

    removed JSoup from the AI

--- src/games/controller/Controller.java
+++ src/games/controller/Controller.java
@@ -7,31 +7,12 @@ import javax.swing.JOptionPane;
 
 public class Controller
 {
-	private ChessAI whiteAI;
-	private ChessAI blackAI;
+	private ChessAI ai;
 	private ChessBoard board;
 	
 	public void start()
 	{	
-		board = new ChessBoard();
-		System.out.println(board.getAsFormattedString());
-		
-		whiteAI = new ChessAI(this, board, "w");
-		blackAI = new ChessAI(this, board, "b");
-		
-		int counter = 0;
-		
-		while (counter < 100)
-		{
-			board.makeMove(whiteAI.getNextMove());
-			System.out.println(board.getAsFormattedString());
-			board.makeMove(blackAI.getNextMove());
-			System.out.println(board.getAsFormattedString());
-			
-			counter++;
-		}
-		
-		//System.out.println(board.getAsFormattedString());
+		ai = new ChessAI(this, board);
 	}
 	
 	public void handleError(Exception error)
