7c3b270d21d3bb9bf8914dd7f83efe27551594e5
Cami <cbrun.mine@gmail.com>
2023-05-05 11:05:33 -0600

    Created very basic interface for playing

--- src/games/controller/Controller.java
+++ src/games/controller/Controller.java
@@ -3,6 +3,8 @@ package games.controller;
 import games.model.chess.ChessAI;
 import games.model.chess.ChessBoard;
 
+import java.util.Scanner;
+
 import javax.swing.JOptionPane;
 
 public class Controller
@@ -14,11 +16,38 @@ public class Controller
 	{	
 		board = new ChessBoard();
 		ai = new ChessAI(this, board);
-
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		while (true)
+		{
+			try
+			{
+				System.out.println(board.getAsFormattedString());
+				String input = scanner.nextLine();
+				makePlayerMove(input);
+				makeAIMove();
+			}
+			catch (InterruptedException error)
+			{
+				handleError(error);
+			}
+		}
 	}
 	
 	public void handleError(Exception error)
 	{
 		JOptionPane.showMessageDialog(null, "Error: " + error.getMessage());
+	}
+	
+	private void makeAIMove() throws InterruptedException
+	{
+		board.makeMove(ai.getNextMove(1000));
+		Thread.sleep(1500);
+	}
+	
+	private void makePlayerMove(String move)
+	{
+		board.makeMove(move);
 	}
 }
