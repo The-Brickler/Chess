2250a4459e9bd1cfa25f216a5d1089b3f8d99824
Cami <cbrun.mine@gmail.com>
2023-05-09 11:54:53 -0600

    Coded in Rook Movement validation

--- src/games/model/chess/ChessRook.java
+++ src/games/model/chess/ChessRook.java
@@ -2,12 +2,10 @@ package games.model.chess;
 
 public class ChessRook extends ChessPiece
 {
-	private int team;
 	
 	public ChessRook(int team, ChessBoard board)
 	{
 		super(team, board);
-		this.team = team;
 	}
 	
 	public String getAsChar()
@@ -25,6 +23,61 @@ public class ChessRook extends ChessPiece
 	@Override
 	public boolean validateMove(int[] firstPos, int[] secondPos)
 	{
-		return false;
+		int rowDifference = secondPos[0] - firstPos[0];
+		int colDifference = secondPos[1] - firstPos[1];
+		
+		if ((rowDifference == 0 || colDifference == 0) && rowDifference != colDifference)
+		{
+			ChessPiece target = super.getPieceAtPosition(secondPos);
+			
+			if (target.getTeam() == super.team)
+			{
+				return false;
+			}
+			
+			else if (Math.abs(rowDifference) == 1 || Math.abs(colDifference) == 1)
+			{
+				return true;
+			}
+			
+			
+			int moveDirection = 0;
+			int axis = 0;
+			if (rowDifference == 0 && colDifference != 1)
+			{
+				moveDirection = colDifference / Math.abs(colDifference);
+				axis = 1;
+			}
+			else if (colDifference == 0 && rowDifference != 1)
+			{
+				moveDirection = rowDifference / Math.abs(colDifference);
+			}
+			
+			for (int index = firstPos[axis] + moveDirection; index != secondPos[axis]; index += moveDirection)
+			{
+				int [] scanPosition = new int[2];
+				
+				if (axis == 0)
+				{
+					scanPosition = new int [] {index, firstPos[1]};
+				}
+				else
+				{
+					scanPosition = new int [] {firstPos[0], index};
+				}
+				
+				ChessPiece scanPiece = super.getPieceAtPosition(scanPosition);
+				if (!scanPiece.getClass().getSimpleName().equals("ChessEmpty"))
+				{
+					return false;
+				}
+			}
+		}
+		else
+		{
+			return false;
+		}
+		
+		return true;
 	}
 }
