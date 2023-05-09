7bd40bba4470737e955a49cc3ac23d7b1f957712
Cami <cbrun.mine@gmail.com>
2023-05-09 13:38:33 -0600

    Created GUI class stubs

---
+++ src/games/view/ChessPanel.java
@@ -0,0 +1,36 @@
+package games.view;
+
+import games.controller.Controller;
+
+import javax.swing.JPanel;
+
+public class ChessPanel extends JPanel
+{
+	
+	private Controller app;
+	
+	public ChessPanel(Controller app)
+	{
+		super();
+		this.app = app;
+		
+		setupPanel();
+		setupListeners();
+		setupLayout();
+	}
+	
+	private void setupPanel()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+}
